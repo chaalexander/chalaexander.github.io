87219ccf9a098636404675fd37e5105eb12d66f1
charlenne alexander <charlennep@gmail.com>
2020-06-25 21:17:46 -0500

    eddited footer and contact

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <div className="footer">
       <h6>
         <i class="far fa-copyright"></i> Cha Alexander{" "}
-        <Link to="https://github.com/chaalexander " target="_blank">
+        <a href="https://github.com/chaalexander " target="_blank">
           <i class="fab fa-github"></i>
-        </Link>{" "}
-        <Link to="https://www.linkedin.com/in/cha-alexander" target="_blank">
+        </a>{" "}
+        <a href="https://www.linkedin.com/in/cha-alexander" target="_blank">
           <i class="fab fa-linkedin"></i>
-        </Link>
+        </a>
       </h6>
     </div>
   );
